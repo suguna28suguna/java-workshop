3a2f7e05cafcdefcde7d815721013adc02e934ca
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-02 14:50:26 +0530

    marks

---
+++ Mark.java
@@ -0,0 +1,10 @@
+public class Mark {
+    public static void main(String[] args) {
+        int[] marks={50,78,69,98,89};
+        for(int mark:marks){
+            System.out.println("marks:"+mark);
+        }
+        
+    }
+}
+
