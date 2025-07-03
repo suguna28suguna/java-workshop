657ff416e5948bd57bf70f9dab52c271d95623b3
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-03 15:05:32 +0530

    Temperature

---
+++ src/com/java/workshop/temparturedata/beans/Floor.java
@@ -0,0 +1,40 @@
+package src.com.java.workshop.temparturedata.beans;
+
+public class Floor {
+    private String name;
+    private int zoneid;
+    private String Stringtype;
+    private double area;
+
+    public void setname(String aName){
+        name=aName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setzoneid(int aZoneid){
+        zoneid=aZoneid;
+    }
+
+    public int getzoneid(){
+       return zoneid;
+    }
+    public void setstringtype(String aStringtype){
+        Stringtype=aStringtype;
+    }
+
+    public String getstringtype() {
+        return Stringtype ;
+    }
+
+    public void setarea(double aArea){
+        area=aArea;
+    }
+
+    public double getarea(){
+       return area;
+    }
+
+}
