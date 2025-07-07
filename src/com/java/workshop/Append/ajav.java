329f42774605b5cf03364cb842c1d2ba72b01c6c
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-07 14:23:51 +0530

    append

---
+++ src/com/java/workshop/Append/ajav.java
@@ -0,0 +1,18 @@
+package src.com.java.workshop.Append;
+
+public class ajav {
+    public static void main(String[] args) {
+        StringBuilder sentence = new StringBuilder();
+        sentence.append(" java");
+        sentence.append(" is");
+        sentence.append(" the");
+        sentence.append( " most");
+        sentence.append(" used");
+        sentence.append(" language");
+        sentence.append(" for");
+        sentence.append(" enterprise");
+        sentence.append(" applications");
+        System.out.println(sentence.toString());
+    }
+
+}
