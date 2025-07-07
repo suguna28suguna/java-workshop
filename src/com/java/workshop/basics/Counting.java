16aea8cf9e7034d9de5bff5363e6b8afc1274f91
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-07 11:49:18 +0530

    counting

--- src/com/java/workshop/basics/Counting.java
+++ src/com/java/workshop/basics/Counting.java
@@ -5,7 +5,9 @@ public class Counting {
     public static void main(String[] args) {
         String sentence="This program shows how can we split a string into multiple string";
         String Words[]=sentence.split(" ");
+        for (String word : Words){
         System.out.println(Words.length);
+        }
     }
 
 }
