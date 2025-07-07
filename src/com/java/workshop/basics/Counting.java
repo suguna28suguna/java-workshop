09dc9e539062ce99b9d38dee95c8000b32296f4b
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-07 11:06:52 +0530

    pro

---
+++ src/com/java/workshop/basics/Counting.java
@@ -0,0 +1,11 @@
+package src.com.java.workshop.basics;
+
+
+public class Counting {
+    public static void main(String[] args) {
+        String sentence="This program shows how can we split a string into multiple string";
+        String Words[]=sentence.split(" ");
+        System.out.println(Words.length);
+    }
+
+}
---
+++ src/com/java/workshop/basics/Pro.java
@@ -0,0 +1,12 @@
+package src.com.java.workshop.basics;
+
+public class Pro {
+    public static void main(String[] args) {
+        String firstname="Suguna";
+        String lastname="E";
+        String Name=firstname.nameconcat(lastname);
+        System.out.println(Name);
+        String finalName=Name.toUpperCase();
+    }
+
+}
