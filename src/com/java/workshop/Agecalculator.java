dd4972c549ce59f4493ac02c2b780d18f73392cb
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-08 10:50:27 +0530

    TIME

--- src/com/java/workshop/Agecalculator.java
+++ src/com/java/workshop/Agecalculator.java
@@ -4,13 +4,13 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Agecalculator {
-    public static void main(String[] args) {
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.println("Enter your birth year:");
-        int yearofBirth = inputScanner.nextInt();
-        int currentyear = LocalDate.now().getYear();
-        int age=currentyear-yearofBirth;
-        System.out.println("your age is:"+age);
+        public static void main(String[] args) {
+        Scanner inputScanner=new Scanner(System.in);
+        System.out.println("Enter your Dateofbirth (YYYY-MM-DD):");
+        String Dateofbirth = inputScanner.nextLine();
+        LocalDate Birthdate=LocalDate.parse(Dateofbirth);
+        int YearofBirth=Birthdate.getYear();
+        int currentYear=LocalDate .now().getYear();
     }
 
 }
