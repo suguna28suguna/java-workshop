310cc6dafb9b69c8292c504f215006ee3cb024cf
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-10 14:25:43 +0530

    list

---
+++ src/main/java/com/java/workshop/collections/ListExample.java
@@ -0,0 +1,22 @@
+package com.java.workshop.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("kobiga");
+        names.add("ajeetha");
+        names.add("madhu");
+        names.add("suguna");
+        System.out.println("Names List:"+names);
+        for(String name:names){
+            System.out.println("Names:"+name);
+        }
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println("Names at index"+ i +"." +names.get(i));
+            
+        }
+    }
+}
