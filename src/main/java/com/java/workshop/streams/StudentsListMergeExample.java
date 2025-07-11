968cd193b6c57abebb5295f108c69310c20dd2c2
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-11 14:24:43 +0530

    merges

--- src/main/java/com/java/workshop/streams/StudentsListMergeExample.java
+++ src/main/java/com/java/workshop/streams/StudentsListMergeExample.java
@@ -2,6 +2,7 @@ package com.java.workshop.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StudentsListMergeExample {
@@ -9,7 +10,7 @@ public class StudentsListMergeExample {
     List<Integer> csRollNos = Arrays.asList(1003,1004,1007,1010,1015);
     List<Integer> csaRollNos = Arrays.asList(1003,1004,1007,1010,1015);
     List<Integer> allRollNos = Stream.concat(csRollNos.stream(),csaRollNos.stream())
-    .sorted().toList();
+    .sorted().collect(Collectors.toList());
     System.out.println(allRollNos);
     }
 }
