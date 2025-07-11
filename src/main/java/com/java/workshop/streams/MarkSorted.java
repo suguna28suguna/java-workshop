ebbc360c50199ce31bd79c54c3bff195f5526584
suguna28suguna <sugunae24cse@vetias.ac.in>
2025-07-11 15:00:52 +0530

    sorted

---
+++ src/main/java/com/java/workshop/streams/MarkSorted.java
@@ -0,0 +1,17 @@
+package com.java.workshop.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MarkSorted {
+    public static void main(String[] args) {
+        List<Integer> marks = Arrays.asList(13,14,17,13,10);
+        List<Integer> sortedmarks = marks.stream()
+        .sorted()
+        .toList();
+        System.out.println("sorted Marks:"+sortedmarks);
+        List<Integer> reverseDec=marks.stream().sorted((a,b) ->b.compareTo(a)).toList();
+        System.out.println("Reverse Sorted Marks:"+reverseDec);
+    }
+
+}
